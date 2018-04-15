0b8ac5ea33798cd2e3577f7c413d5e5bba8347b3
= <nick.hadley31@gmail.com>
2018-04-15 01:48:01 -0400

    started a number select wheel

--- sketch.js
+++ sketch.js
@@ -1,5 +1,5 @@
-const CANVAS_WIDTH = 700;
-const CANVAS_HEIGHT = 700;
+const CANVAS_WIDTH = 900;
+const CANVAS_HEIGHT = 900;
 
 const TEST_SUDOKU = [
 	6, 0, 9, 0, 0, 8, 3, 0, 2, 
@@ -13,18 +13,109 @@ const TEST_SUDOKU = [
 	2, 0, 0, 0, 0, 0, 5, 9, 4
 ];
 
+
 let sudoku;
+let numSelect;
 
 function setup() 
 {
 	createCanvas(CANVAS_WIDTH,  CANVAS_HEIGHT);
 
-	sudoku = new Sudoku (TEST_SUDOKU);
+	sudoku = new Sudoku (100, 100, 700, TEST_SUDOKU);
+	numSelect = new NumberSelectWheel (85);
 }
 
 function draw ()
 {
 	background(51);
 
-	sudoku.show (100, 100, 500, 500);
+	sudoku.show ();
+	numSelect.show();
+}
+
+
+
+function mousePressed()
+{
+	numSelect.setPosition (mouseX, mouseY);
+	numSelect.reveal();
+	return false;
+}
+
+function mouseReleased()
+{
+	numSelect.select();
+	numSelect.hide();
+	return false;
+}
+
+
+class NumberSelectWheel
+{
+	constructor (size)
+	{
+		this.x = 0;
+		this.y = 0;
+		this.size = size;
+		this.enabled = false;
+	}
+}
+
+NumberSelectWheel.prototype.reveal = function ()
+{
+	this.enabled = true;
+};
+
+NumberSelectWheel.prototype.hide = function ()
+{
+	this.enabled = false;
+};
+
+NumberSelectWheel.prototype.show = function ()
+{
+	if (!this.enabled)
+		return;
+
+	textAlign(CENTER, CENTER);
+
+	fill (100, 100, 100, 100);
+	ellipse(this.x, this.y, this.size * 2);
+
+	stroke (0);
+	strokeWeight (3);
+	line (this.x, this.y, mouseX, mouseY);
+
+	for (let i = 0; i < 10; i++)
+	{
+		let angle = i * TWO_PI / 10;
+
+		let tx = this.x + this.size * sin(angle);
+		let ty = this.y - this.size * cos(angle);
+
+		let d = dist(mouseX, mouseY, tx, ty);
+
+		textSize(this.size - clamp (d, 0, this.size / 2));
+		text (i, tx, ty);
+	}
+
+};
+
+NumberSelectWheel.prototype.setPosition = function (x, y)
+{
+	this.x = x;
+	this.y = y;
+};
+
+NumberSelectWheel.prototype.select = function ()
+{
+	print ("select");
+};
+
+function clamp (a, min, max)
+{
+	if (a < min)
+		return min;
+	if (a > max)
+		return max;
+	return a;
 }
--- sudoku.js
+++ sudoku.js
@@ -1,8 +1,12 @@
 
 class Sudoku
 {
-	constructor (arr)
+	constructor (x, y, size, arr)
 	{
+		this.x = x;
+		this.y = y;
+		this.size = size;
+
 		if (arr)
 		{
 			this.values = arr;
@@ -34,7 +38,7 @@ Sudoku.prototype.getRow = function (n)
 
 	for (let i = 0; i < 9; i++)
 	{
-		arr.push (getValue (i,  n));
+		arr.push (this.getValue (i,  n));
 	}
 
 	return arr;
@@ -49,7 +53,7 @@ Sudoku.prototype.getCol = function (n)
 
 	for (let i = 0; i < 9; i++)
 	{
-		arr.push (getValue (n,  i));
+		arr.push (this.getValue (n,  i));
 	}
 
 	return arr;
@@ -57,8 +61,8 @@ Sudoku.prototype.getCol = function (n)
 
 Sudoku.prototype.getBox = function (n)
 {
-	let startX = floor(getX (n) / 3) * 3;
-	let startY = floor(getY (n) / 3) * 3;
+	let startX = floor(this.getX (n) / 3) * 3;
+	let startY = floor(this.getY (n) / 3) * 3;
 
 	let arr = [];
 
@@ -66,11 +70,21 @@ Sudoku.prototype.getBox = function (n)
 	{
 		for (let j = 0; j < 3; j++)
 		{
-			arr.push (getValue (startX + j,  startY + i));
+			arr.push (this.getValue (startX + j,  startY + i));
 		}
 	}
 
 	return arr;
+};
+
+Sudoku.prototype.getCellX = function (px)
+{
+	return floor((px - this.x) / this.size * 9);
+};
+
+Sudoku.prototype.getCellY = function (py)
+{
+	return floor((py - this.y) / this.size * 9);
 };
 
 Sudoku.prototype.testValue = function (i, n)
@@ -81,9 +95,9 @@ Sudoku.prototype.testValue = function (i, n)
 		!contains (this.getCol (i), n);
 };
 
-Sudoku.prototype.show = function (x, y, s)
+Sudoku.prototype.show = function ()
 {
-	let size = s / 9.0;
+	let cellSize = this.size / 9.0;
 
 
 	// Draw small lines
@@ -94,12 +108,12 @@ Sudoku.prototype.show = function (x, y, s)
 	for (let i = 1; i < 9; i += 3)
 	{
 		// Draw horizontal lines
-		line (x, y + size * i, x + s, y + size * i);
-		line (x, y + size * (i + 1), x + s, y + size * (i + 1));
+		line (this.x, this.y + cellSize * i, this.x + this.size, this.y + cellSize * i);
+		line (this.x, this.y + cellSize * (i + 1), this.x + this.size, this.y + cellSize * (i + 1));
 
 		// Draw vertical lines
-		line (x + size * i, y, x + size * i, y + s);
-		line (x + size * (i + 1), y, x + size * (i + 1), y + s);
+		line (this.x + cellSize * i, this.y, this.x + cellSize * i, this.y + this.size);
+		line (this.x + cellSize * (i + 1), this.y, this.x + cellSize * (i + 1), this.y + this.size);
 	}
 
 	// Draw large lines
@@ -110,36 +124,35 @@ Sudoku.prototype.show = function (x, y, s)
 	for (let i = 0; i <= 9; i += 3)
 	{
 		// Draw horizontal line
-		line (x, y + size * i, x + s, y + size * i);
+		line (this.x, this.y + cellSize * i, this.x + this.size, this.y + cellSize * i);
 
 		// Draw vertical line
-		line (x + size * i, y, x + size * i, y + s);
+		line (this.x + cellSize * i, this.y, this.x + cellSize * i, this.y + this.size);
 	}
 
 
 	// Draw numbers
 
+	fill (170);
+	strokeWeight (3);
+
+	textSize(cellSize * 0.8);
+	textAlign(CENTER, CENTER);
+
 	for (let i = 0; i < 9; i++)
 	{
 		for (let j = 0; j < 9; j++)
 		{
-			let s = sudoku.getValue (i,  j);
-			if (s > 0)
+			let val = this.getValue (i,  j);
+			if (val > 0)
 			{
-				strokeWeight (1);
-
-				textSize(SQUARE_SIZE * 0.7);
-				textAlign(CENTER, CENTER);
-
-				let px = getPixelX (i);
-				let py = getPixelY (j);
-
-				text (s,  px + SQUARE_SIZE / 2, py + SQUARE_SIZE / 2);
+				let tx = this.x + (i + 0.5) * cellSize;
+				let ty = this.y + (j + 0.5) * cellSize;
+				text (val, tx, ty);
 			}
 		}
 	}
 };
-
 
 
 
@@ -152,15 +165,18 @@ function getIndex (x,  y)
 	return x + 9 * y;
 }
 
+
 function getX (i)
 {
 	return floor(i % 9);
 }
 
+
 function getY (i)
 {
 	return floor(i / 9);
 }
+
 
 function contains (arr, n)
 {
